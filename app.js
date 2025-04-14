972f8eed5f2208cfcc4314be4c78684cf608f6cf
bwaldner <bwaldner@trinity.edu>
2025-04-14 12:04:24 -0500

    setupstuff

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const indexRouter = require('./routes/index');
 require('dotenv').config();
+
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/',indexRouter);
 
 // Import routes
 const sessionRoutes = require('./routes/sessionRoutes');
 app.use('/session', sessionRoutes);
 
 // Default route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+//app.get('/', (req, res) => {
+   // res.sendFile(path.join(__dirname, 'public/index.html'));
+//});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
