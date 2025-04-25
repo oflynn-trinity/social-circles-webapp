f1c40f4b6246e07f1ba82244858e8e11315c431c
oflynn-trinity <oflynn@trinity.edu>
2025-04-25 12:02:22 -0500

    updated sql script

--- Database/database.sql
+++ Database/database.sql
@@ -1,5 +1,3 @@
--- Create the database and switch to it
-CREATE DATABASE IF NOT EXISTS GameDB;
 USE GameDB;
 
 DROP TABLE IF EXISTS Characters;
@@ -12,17 +10,17 @@ CREATE TABLE Account (
     username VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     score INT DEFAULT 0,
-    active TINYINT(1) DEFAULT 1,  -- 0 = inactive, 1 = active
+    active INT DEFAULT 1
 );
 
 CREATE TABLE Characters (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     thumbnail_url VARCHAR(150),
-    likes_compliments TINYINT(1) DEFAULT 0,
-    likes_help TINYINT(1) DEFAULT 0,
-    likes_events TINYINT(1) DEFAULT 0,
-    FOREIGN KEY (account_id) REFERENCES Account(id) ON DELETE SET NULL
+    likes_compliments INT DEFAULT 0,
+    likes_help INT DEFAULT 0,
+    likes_events INT DEFAULT 0
 );
 
-
+INSERT INTO Account (username, password)
+VALUES ('omen',1234)
